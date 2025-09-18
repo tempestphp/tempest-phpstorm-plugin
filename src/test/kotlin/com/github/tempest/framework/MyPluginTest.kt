ff63b9a2dbf5ddf4c17d35d35bc23f45a8fa779c
Mark <magyarheaven@gmail.com>
2025-09-18 18:08:00 +0200

    feat: add ktfmt-gradle plugin for consistent formatting
    Can be invoked via `gradlew ktfmtCheck` and `gradlew ktfmtFormat`

--- src/test/kotlin/com/github/tempest/framework/MyPluginTest.kt
+++ src/test/kotlin/com/github/tempest/framework/MyPluginTest.kt
@@ -1,12 +1,12 @@
 package com.github.tempest.framework
 
+import com.github.tempest.framework.services.MyProjectService
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.tempest.framework.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
