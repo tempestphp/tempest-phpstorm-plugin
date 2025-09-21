e025b06a8d020250495b1216f16fdce42d92f717
Dmitriy Derepko <xepozz@list.ru>
2025-09-21 19:52:49 +0400

    chore: add component name prefix

--- src/main/kotlin/com/github/tempest/framework/TempestFrameworkUtil.kt
+++ src/main/kotlin/com/github/tempest/framework/TempestFrameworkUtil.kt
@@ -1,6 +1,6 @@
 package com.github.tempest.framework
 
 object TempestFrameworkUtil {
-    const val TEMPLATE_PREFIX = ".view.php"
     const val TEMPLATE_SUFFIX = ".view.php"
+    const val COMPONENT_NAME_PREFIX = "x-"
 }
