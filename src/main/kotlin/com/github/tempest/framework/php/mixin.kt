0a0ddeb746fb7f4aad9e17706bd8216cb5d44ceb
Dmitriy Derepko <xepozz@list.ru>
2025-09-22 20:41:09 +0400

    fix: filter defined and re-defined variables

--- src/main/kotlin/com/github/tempest/framework/php/mixin.kt
+++ src/main/kotlin/com/github/tempest/framework/php/mixin.kt
@@ -7,5 +7,7 @@ import com.jetbrains.php.lang.psi.elements.Variable
 fun PhpFile.getPhpViewVariables(): Set<Variable> {
     return PsiTreeUtil.findChildrenOfType(this, Variable::class.java)
         .filter { it.useScope == useScope }
+        .filter { !it.isDeclaration }
+        .distinctBy { it.name }
         .toSet()
 }
