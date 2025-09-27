a85a01fb8c62c3bb891a182d026f212da4c6102f
Dmitriy Derepko <xepozz@list.ru>
2025-09-27 22:35:56 +0400

    fix: filter non-commands

--- src/main/kotlin/com/github/tempest/framework/console/run/ConsoleCommandLineMarkerProvider.kt
+++ src/main/kotlin/com/github/tempest/framework/console/run/ConsoleCommandLineMarkerProvider.kt
@@ -1,5 +1,6 @@
 package com.github.tempest.framework.console.run
 
+import com.github.tempest.framework.php.getConsoleCommandName
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.psi.PsiElement
 import com.jetbrains.php.lang.psi.elements.Method
@@ -7,6 +8,7 @@ import com.jetbrains.php.lang.psi.elements.Method
 class ConsoleCommandLineMarkerProvider : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement) = when {
         element !is Method -> null
+        element.getConsoleCommandName() == null -> null
         else -> Info(TempestRunTargetAction(element))
     }
 }
