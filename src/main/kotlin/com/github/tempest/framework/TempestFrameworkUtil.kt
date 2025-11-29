30dad7999167ad46814fb7356204ba572447bdea
Mark <magyarheaven@gmail.com>
2025-11-29 08:04:06 +0100

    fix: skip built-in attributes so the IDE can resolve the symbols
    Skips reference creation for built-in attributes

--- src/main/kotlin/com/github/tempest/framework/TempestFrameworkUtil.kt
+++ src/main/kotlin/com/github/tempest/framework/TempestFrameworkUtil.kt
@@ -3,4 +3,12 @@ package com.github.tempest.framework
 object TempestFrameworkUtil {
     const val TEMPLATE_SUFFIX = ".view.php"
     const val COMPONENT_NAME_PREFIX = "x-"
+
+    val BUILT_IN_DIRECTIVE_ATTRIBUTES = setOf(
+        ":if",
+        ":elseif",
+        ":else",
+        ":foreach",
+        ":forelse",
+    )
 }
